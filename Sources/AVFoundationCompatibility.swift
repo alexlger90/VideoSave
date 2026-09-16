4e535df7826463e3bcc452a049e85de365da4d7a
alexlger90 <alexander.liepe90@gmail.com>
2026-09-16 04:49:19 +0200

    Remove obsolete AVFoundation compatibility file

--- Sources/AVFoundationCompatibility.swift
+++
@@ -1,6 +0,0 @@
-import Foundation
-
-// AVURLAsset accepts this options dictionary key for HTTP request headers.
-// Xcode 16.4 no longer exposes the old SDK symbol to Swift, so keep the
-// string key locally instead of depending on that removed declaration.
-let AVURLAssetHTTPHeaderFieldsKey = "AVURLAssetHTTPHeaderFieldsKey"
